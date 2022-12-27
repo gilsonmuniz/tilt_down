c6e74f5dc56f4f7ad0fc90d2abedc860920bb9f1
Gilson Muniz <59257835+GilsonMuniz@users.noreply.github.com>
2022-12-27 12:47:31 -0300

    Update controlling_test.py

--- controlling_test.py
+++ controlling_test.py
@@ -5,9 +5,6 @@ Controlador do teste de Tilt Down  |
 -----------------------------------+
 """
 
-# To get the cursor coordinates: http://www.brenz.net/snippets/xy.asp#:~:text=Tracking%20the%20Cursor%20X%2FY%20Coordinates&text=On%20this%20page%2C%20press%20and,with%20the%20%27z%27%20accesskey.
-# (X, +70Y)
-
 import pyautogui
 from time import sleep
 
@@ -24,16 +21,16 @@ pyautogui.click()
 daq_digital_inputs_reference = pyautogui.locateCenterOnScreen('images/daq_digital_inputs_reference.png')
 pyautogui.moveTo(daq_digital_inputs_reference.x + 150, daq_digital_inputs_reference.y + 30, 0.2) # Changing DAQ's name
 pyautogui.click()
-# for _ in range(7): # Removing "line0:3"
-#     pyautogui.press('backspace')
-# pyautogui.write('Dev3')
+for _ in range(7): # Removing "line0:3"
+    pyautogui.press('backspace')
+pyautogui.write('Dev3')
 
 daq_analog_inputs_reference = pyautogui.locateCenterOnScreen('images/daq_analog_inputs_reference.png')
 pyautogui.moveTo(daq_analog_inputs_reference.x + 150, daq_analog_inputs_reference.y + 30, 0.2) # Changing DAQ's name
 pyautogui.click()
-# for _ in range(5): # Removing "ai6:7"
-#     pyautogui.press('backspace')
-# pyautogui.write('Dev3')
+for _ in range(5): # Removing "ai6:7"
+    pyautogui.press('backspace')
+pyautogui.write('Dev3')
 
 run_button = pyautogui.locateCenterOnScreen('images/run_button.png')
 pyautogui.moveTo(run_button) # "Run" button
@@ -64,45 +61,45 @@ def press_plus_vertical_position():
 # ---------------------------------- Align horizontal Tilt Down axis
 
 tilt_down_horizontal_aligned = False
-# while tilt_down_horizontal_aligned == False:
-#         if pyautogui.locateCenterOnScreen('images/horizontal_1.9V.png') is not None: # Picture "Horizontal: 1.9"
-#             tilt_down_horizontal_aligned = True
-#         else:
-#             press_plus_horizontal_position()
-#             tilt_down_horizontal_aligned = False
+while tilt_down_horizontal_aligned == False:
+        if pyautogui.locateCenterOnScreen('images/horizontal_1.9V.png') is not None: # Picture "Horizontal: 1.9"
+            tilt_down_horizontal_aligned = True
+        else:
+            press_plus_horizontal_position()
+            tilt_down_horizontal_aligned = False
 
 # ---------------------------------- Align vertical Tilt Down axis
 
 tilt_down_vertical_aligned = False
-# while tilt_down_vertical_aligned == False:
-#         if pyautogui.locateCenterOnScreen('images/vertical_1.3V.png') is not None: # Picture "Vertical: 1.3"
-#             tilt_down_vertical_aligned = True
-#         else:
-#             press_plus_vertical_position()
-#             tilt_down_vertical_aligned = False
+while tilt_down_vertical_aligned == False:
+        if pyautogui.locateCenterOnScreen('images/vertical_1.3V.png') is not None: # Picture "Vertical: 1.3"
+            tilt_down_vertical_aligned = True
+        else:
+            press_plus_vertical_position()
+            tilt_down_vertical_aligned = False
 
-# tilt_down_button = pyautogui.locateCenterOnScreen('images/tilt_down_button.png')
+tilt_down_button = pyautogui.locateCenterOnScreen('images/tilt_down_button.png')
 pyautogui.click() # Saving Tilt Down position (1.9, 1.3)
 
 # ---------------------------------- Align horizontal Drive axis
 
 drive_horizontal_aligned = False
-# while drive_horizontal_aligned == False:
-#         if pyautogui.locateCenterOnScreen('images/horizontal_3.4V.png') is not None: # Picture "Horizontal: 3.4"
-#             drive_horizontal_aligned = True
-#         else:
-#             press_plus_horizontal_position()
-#             drive_horizontal_aligned = False
+while drive_horizontal_aligned == False:
+        if pyautogui.locateCenterOnScreen('images/horizontal_3.4V.png') is not None: # Picture "Horizontal: 3.4"
+            drive_horizontal_aligned = True
+        else:
+            press_plus_horizontal_position()
+            drive_horizontal_aligned = False
 
 # ---------------------------------- Align vertical Drive axis
 
 drive_vertical_aligned = False
-# while drive_vertical_aligned == False:
-#         if pyautogui.locateCenterOnScreen('images/vertical_3.4V.png') is not None: # Picture "Vertical: 3.4"
-#             drive_vertical_aligned = True
-#         else:
-#             press_plus_horizontal_position()
-#             drive_vertical_aligned = False
+while drive_vertical_aligned == False:
+        if pyautogui.locateCenterOnScreen('images/vertical_3.4V.png') is not None: # Picture "Vertical: 3.4"
+            drive_vertical_aligned = True
+        else:
+            press_plus_horizontal_position()
+            drive_vertical_aligned = False
 
 tilt_down_button = pyautogui.locateCenterOnScreen('images/drive_button.png')
 pyautogui.click() # Saving Drive position (3.4, 3.4)
@@ -150,7 +147,7 @@ for _ in range(3):
     pyautogui.moveTo(r_button)
     pyautogui.click()
 
-sleep(50) # Time to wait complete signal appears on the graph
+sleep(70) # Time to wait complete signal appears on the graph
 pyautogui.screenshot('graphs/3_cycles/{}.png'.format(sample_name))
 
 # ==========================================| 10 Cycles Test |========================================== #
@@ -206,9 +203,9 @@ comecar_button = pyautogui.locateCenterOnScreen('images/comecar_button.png')
 pyautogui.moveTo(comecar_button)
 pyautogui.click()
 
-sleep(280) # Time to wait complete signal appears on the graph (300s)
+sleep(280)
 pyautogui.screenshot('graphs/10_cycles/{}.png'.format(sample_name))
 
 # ============================================| End of Test |=========================================== #
 
-pyautogui.confirm('Teste da {} finalizado.'.format(sample_name))
+pyautogui.confirm('Teste da amostra {} finalizado.'.format(sample_name))
